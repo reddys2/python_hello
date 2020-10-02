a04c7f112d724d6895044110a43937a7aa48dc37
GaurikaDwivedi <72243047+GaurikaDwivedi@users.noreply.github.com>
2020-10-02 19:01:50 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,1 @@
-# This is the sample hello python print script
-def print_hello():
-    print "Hello World"
-    return "Hello World"
-
-
-def main():
-    print_hello()
-
-
-if __name__ == '__main__':
-    main()
+print("Hello world")
